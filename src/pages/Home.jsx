19c966c4849e5d01f6a6c76658827d42bb5fee03
Marc <nguyenvannhv26@gmail.com>
2022-10-18 07:42:37 +0700

    merge

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -32,7 +32,7 @@ const Home = () => {
       </div>
       <div className="row mt-24">
         <div className="col-3 ">
-          <div className="rounded-4 bg-report-1 d-flex justify-content-center align-items-center py-24">
+          <div className="rounded-4 bg-report-1 d-flex justify-content-center align-items-center py-12">
             <div className=" py-12 text-center">
               <div className="p-12 border rounded-circle ">
                 <IoBookSharp className="fs-1 text-report-1-bold opacity-75" />
